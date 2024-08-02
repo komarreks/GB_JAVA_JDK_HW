ef72699a64c2f00a608438d12b194a0ceea0ddc8
Sergey Komarov <skomar-off@yandex.ru>
2024-08-02 11:11:08 +0500

    Монти Холл

--- HomeWork/src/main/java/montyHoulParadox/Main.java
+++ HomeWork/src/main/java/montyHoulParadox/Main.java
@@ -8,6 +8,7 @@ public class Main {
     private static HashSet<Door> openedDoors = new HashSet<>();
 
     private static HashMap<String, Integer> statistic = new HashMap<>();
+    private static HashMap<Integer, String> steps = new HashMap<>();
 
     public static void main(String[] args) {
         List<Door> doors = createDoors();
@@ -18,22 +19,32 @@ public class Main {
 
         System.out.println("Создано дверей: " + COUNT_DOORS);
 
+        int step = 0;
+
         while (!win){
             int doorNumber = new Random().nextInt(0, COUNT_DOORS);
 
             Door door = doors.get(doorNumber);
 
             if (!openedDoors.contains(door)){
+                step++;
                 System.out.println("Компьютер выбрал " + door.toString());
                 if (door.isGold()){
-                    System.out.println("Компьюетр угадал с " + (statistic.get("Неверно") + 1) + " попыток дверь с золотом");
+                    System.out.println("Компьютер угадал с " + (statistic.get("Неверно") + 1) + " попыток дверь с золотом");
+                    steps.put(step , "угадал");
                     win = true;
                 }else {
                     statistic.put("Неверно", statistic.get("Неверно") + 1);
                     System.out.println("... там нет золота");
                     openedDoors.add(door);
+                    steps.put(step, "не верно");
                 }
             }
+        }
+
+        System.out.println("Статистика шагов:");
+        for (int key: steps.keySet()){
+            System.out.println("шаг: " + key + ", результат - " + steps.get(key));
         }
 
     }
